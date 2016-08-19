4acc181d73055fd857f1bc4000380fd6272e1603
adniltonweb <adniltonweb@gmail.com>
2016-08-19 10:33:54 -0300

    Arquivos Inicias do Projeto

---
+++ view/painel/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>Imobiliar</title>
+</head>
+     
+     <!-- Links de Ferramentas de Estilos -->
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css">
+
+<body class="fundo">
+
+<section>
+
+	<!--Bloco do Logo e Titulo -->
+	<div class="logo-tit">
+		<img class="logo"src="img-icon/logo.png">
+		<div class="titulo">IMOBILIAR</div>
+		<div class="version">versão - 0.10</div>
+	</div>
+
+	<!--Bloco do Login e Senha -->
+	<div class="aut">
+		<form>
+		<input type="text" class="form-control" name="email" placeholder="EMAIL">
+		<input type="password" class="form-control" name="senha" placeholder="PASSWORD">
+		<button type="button" class="btn btn-warning btn-lg">OK</button>
+		</form>
+		<div class="forget"> Esqueceu sua senha ? </div>
+		<div class="new"> Nova Conta </div>
+	</div>
+
+</section>
+
+</body>
+</html>
