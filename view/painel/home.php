6bd14ada0ec7a98f5e808365e9084f8fc5d2853a
adniltonweb <adniltonweb@gmail.com>
2016-08-21 22:16:45 -0300

    terminando index.php

---
+++ view/painel/home.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+	<title>Imobiliar</title>
+</head>
+
+     <!-- Links de Ferramentas de Estilos  -->
+    <link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/css/index.css">
+	<link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/frameworks/bootstrap/bootstrap.css">
+	<script type="text/javascript" src="/imobiliar/view/painel/frameworks/angular/angular.js"></script>
+	<link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/css/home.css">
+<body>
+
+<div id="topo"></div>
+
+</body>
+</html>
+
+
+
+
--- view/painel/index.php
+++ view/painel/index.php
@@ -1,38 +1,130 @@
-<html>
+<html ng-app="app">
 <head>
 	<title>Imobiliar</title>
 </head>
-	<?php
-	$css = $data['baseDir'];
-	
-	 ?>
-     <!-- Links de Ferramentas de Estilos -->
-	<link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/css/index.css">
-	<link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/bootstrap/bootstrap.css">
 
-<body class="fundo">
+     <!-- Links de Ferramentas de Estilos  -->
+    <link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/css/index.css">
+	<link rel="stylesheet" type="text/css" href="/imobiliar/view/painel/frameworks/bootstrap/bootstrap.css">
+	<script type="text/javascript" src="/imobiliar/view/painel/frameworks/angular/angular.js"></script>
+
+	<script type="text/javascript">
+	var app = angular.module("app",[]);
+	app.controller("appController", function($scope){
+
+		$scope.conta = true;
+		$scope.newconta = false;
+		$scope.senha = false;
+
+	});
+	</script>
+	
+
+
+<body class="fundo"  ng-controller="appController">
 
 <section>
 
 	<!--Bloco do Logo e Titulo -->
 	<div class="logo-tit">
-		<img class="logo" src="/imobiliar/view/painel/img-icon/logo.png">
+		<img class="logo" src="/imobiliar/view/painel/img/logo.png">
 		<div class="titulo">IMOBILIAR</div>
 		<div class="version">versão - 0.10</div>
 	</div>
 
 	<!--Bloco do Login e Senha -->
-	<div class="aut">
-		<form>
-		<input type="text" class="form-control" name="email" placeholder="EMAIL">
-		<input type="password" class="form-control" name="senha" placeholder="PASSWORD">
-		<button type="button" class="btn btn-warning btn-lg">OK</button>
+	<div class="aut" ng-show="conta" >
+	
+		<form action="painel/home" method="post">
+			<input type="text" class="form-control" name="email" placeholder="EMAIL" autocomplete="off">
+			<input type="password" class="form-control" name="senha" placeholder="SENHA">
+			<button type="submit" class="btn btn-warning btn-lg botao">OK</button>
 		</form>
-		<div class="forget"> Esqueceu sua senha ? </div>
-		<div class="new"> Nova Conta </div>
+			
+		<div class="forget"><a href="" ng-click="senha = true; conta=false"> Esqueceu sua senha ? </a> </div>
+		<div class="new"><a href="" ng-click="conta = false; newconta = true">Nova Conta</a></div>
 	</div>
+
+	<!--Bloco de Esqueceu sua senha -->
+	<div class="aut" ng-show="senha" >
+	 	<form action="painel/senha" method="post">
+			 <table>
+			
+			 <tr>
+			 	<td>
+		  	<input type="text" class="form-control" name="email" placeholder="EMAIL" ng-model="user.name">
+				</td>
+			</tr>	
+		
+			<tr>
+				<td>
+			<br><button type="submit" class="btn btn-primary form-control">Enviar</button>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+			<br><button type="submit" class="btn btn-danger form-control" ng-click="conta = true; senha = false">Voltar</button>
+				</td>
+			</tr>
+		
+		</form>
+			</table>
+	</div>
+		
+	
+	<!--Nova Conta -->
+	<div class="aut newconta" ng-show="newconta">
+		<table>	
+			
+			<form action="painel/newconta" method="post">
+			
+			<tr>
+				<td>
+			<input type="text" class="form-control" name="email" placeholder="EMAIL">
+				</td>
+			</tr>
+		
+			<tr>
+				<td>
+			<input type="password" class="form-control" name="senha" placeholder="SENHA">
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+			<input type="text" class="form-control" name="senha" placeholder="CRESCI">
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+			<input type="text" class="form-control phone-ddd-mask" name="senha" placeholder="TELEFONE" >
+				</td>
+			</tr>
+		
+			<tr>
+				<td>
+			</br><button type="submit" class="form-control btn btn-primary">Registrar</button>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+			</br><button type="button" class="form-control btn btn-danger" ng-click="conta = true; newconta = false;">Voltar</button>
+				</td>
+			</tr>
+		
+		</form>
+		</table>
+		
+	</div>
+
 
 </section>
 
 </body>
 </html>
+
+
+
